7e32fa2b254b440895e055a285bb7201ba914be8
KLm0n <lidingyang@126.com>
2024-12-19 22:53:01 +0800

    fix login

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,3 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-
 <template>
   <div class="container">
     <header>
@@ -7,77 +5,68 @@
     </header>
     <div class="pane">
       <div class="wrapper" :class="{ active }">
-        <div class="container">
-          <header>
-            <h2 class="logo">SSEOJ</h2>
-          </header>
-          <div class="pane">
-            <div class="wrapper" :class="{ active }">
-              <!-- Login Form -->
-              <div class="form_box login">
-                <h2>Login</h2>
-                <form @submit.prevent="doLogin">
-                  <div class="input_box">
-                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
-                    <input type="text" v-model="email" required />
-                    <label>Email</label>
-                  </div>
-                  <div class="input_box">
-                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-                    <input type="password" v-model="password" required />
-                    <label>Password</label>
-                  </div>
-                  <div class="remember_forgot">
-                    <label><input type="checkbox">Remember me</label>
-                    <a href="#">Forgot Password?</a>
-                  </div>
-                  <button type="submit" class="btn">Login</button>
-                  <div class="login_register">
-                    <p>Don't have an account? <label class="register_link" @click="showRegister">Register</label></p>
-                  </div>
-                </form>
-              </div>
-
-              <!-- Registration Form -->
-              <div class="form_box register">
-                <h2>Registration</h2>
-                <form @submit.prevent="doRegister">
-                  <div class="input_box">
-                    <span class="icon"><ion-icon name="person"></ion-icon></span>
-                    <input type="text" v-model="username" required>
-                    <label>Username</label>
-                  </div>
-                  <div class="input_box">
-                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
-                    <input type="text" v-model="registerEmail" required>
-                    <label>Email</label>
-                  </div>
-                  <div class="input_box_verfication">
-                    <span class="icon"><ion-icon name="pencil"></ion-icon></span>
-                    <input type="text" v-model="verificationCode" required>
-                    <label>Verification Code</label>
-                  </div>
-                  <div>
-                    <button type="submit" class="btn_verification">Send</button>
-                  </div>
-                  <div class="input_box">
-                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-                    <input type="password" v-model="registerPassword" required>
-                    <label>Password</label>
-                  </div>
-                  <div class="input_box">
-                    <span class="icon"><ion-icon name="alert-circle"></ion-icon></span>
-                    <input type="password" v-model="repassword" required>
-                    <label>Repassword</label>
-                  </div>
-                  <button type="sumit" class="btn">Register</button>
-                  <div class="login_register">
-                    <p>Already have an account? <label class="login_link" @click="showLogin">Login</label></p>
-                  </div>
-                </form>
-              </div>
+        <!-- Login Form -->
+        <div class="form_box login">
+          <h2>Login</h2>
+          <form @submit.prevent="doLogin">
+            <div class="input_box">
+              <span class="icon"><ion-icon name="mail"></ion-icon></span>
+              <input type="text" v-model="email" required />
+              <label>Email</label>
             </div>
-          </div>
+            <div class="input_box">
+              <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+              <input type="password" v-model="password" required />
+              <label>Password</label>
+            </div>
+            <div class="remember_forgot">
+              <label><input type="checkbox">Remember me</label>
+              <a href="#">Forgot Password?</a>
+            </div>
+            <button type="submit" class="btn">Login</button>
+            <div class="login_register">
+              <p>Don't have an account? <label class="register_link" @click="showRegister">Register</label></p>
+            </div>
+          </form>
+        </div>
+
+        <!-- Registration Form -->
+        <div class="form_box register">
+          <h2>Registration</h2>
+          <form @submit.prevent="doRegister">
+            <div class="input_box">
+              <span class="icon"><ion-icon name="person"></ion-icon></span>
+              <input type="text" v-model="username" required>
+              <label>Username</label>
+            </div>
+            <div class="input_box">
+              <span class="icon"><ion-icon name="mail"></ion-icon></span>
+              <input type="text" v-model="registerEmail" required>
+              <label>Email</label>
+            </div>
+            <div class="input_box_verfication">
+              <span class="icon"><ion-icon name="pencil"></ion-icon></span>
+              <input type="text" v-model="verificationCode" required>
+              <label>Verification Code</label>
+            </div>
+            <div>
+              <button type="submit" class="btn_verification">Send</button>
+            </div>
+            <div class="input_box">
+              <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+              <input type="password" v-model="registerPassword" required>
+              <label>Password</label>
+            </div>
+            <div class="input_box">
+              <span class="icon"><ion-icon name="alert-circle"></ion-icon></span>
+              <input type="password" v-model="repassword" required>
+              <label>Repassword</label>
+            </div>
+            <button type="sumit" class="btn">Register</button>
+            <div class="login_register">
+              <p>Already have an account? <label class="login_link" @click="showLogin">Login</label></p>
+            </div>
+          </form>
         </div>
       </div>
     </div>
