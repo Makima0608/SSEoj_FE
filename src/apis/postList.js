4f62693c1f109e8257df905d529c193e8e191b96
Makima0608 <1391218559@qq.com>
2024-12-30 22:41:39 +0800

    12.30 斜杠

--- src/apis/postList.js
+++ src/apis/postList.js
@@ -1,7 +1,7 @@
 import request from '@/utils/http'
 export const getPostListAPI = (params) => {
   return request({
-      url: '/api/post/list',
+      url: '/api/post/list/',
       params,
   })
 }
