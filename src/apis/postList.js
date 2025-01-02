88eeff1883feb7060f8266c68b3199d25ff44256
SaMeTa-cxl <chenxlei23@mail2.sysu2.edu.cn>
2025-01-02 15:44:26 +0800

    finish post func on home page

--- src/apis/postList.js
+++ src/apis/postList.js
@@ -5,3 +5,22 @@ export const getPostListAPI = (params) => {
       params,
   })
 }
+
+export const getHotPostAPI = (params) => {
+  return request({
+    url: '/api/post/hot_post/',
+    params,
+  })
+}
+
+export const getSubscribePostAPI = () => {
+  return request({
+    url: '/api/post/subscribe_post/',
+  })
+}
+
+export const getMyPostAPI = () => {
+  return request({
+    url: '/api/post/my_post/',
+})
+}
