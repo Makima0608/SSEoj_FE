619245676a9bfabf386f728e5bf8094db16fe672
Makima0608 <1391218559@qq.com>
2025-01-14 02:08:45 +0800

    1.14 post comments改动

--- src/apis/comment.js
+++ src/apis/comment.js
@@ -1,16 +1,43 @@
 import request from '@/utils/http';
 
-export const getCommentAPI = (params) => {
-  const id = params.id;
+export const getPrimaryCommentAPI = (params) => {
+  const post_id = params.id;
   return request({
-    url: `/api/post/${id}/comments/`,
+    url: `/api/post/${post_id}/comments/`,
     method: 'GET',
-    data:{
+    params:{
       page:params.page_num,
       page_size:params.page_size
     }
   });
 };
+
+export const getSecondCommentAPI = (params) => {
+  const post_id = params.post_id;
+  const comment_id = params.comment_id;
+  // console.log(`/api/post/${post_id}/comments/${comment_id}/`); // 打印 URL
+  return request({
+    url: `/api/post/${post_id}/comments/${comment_id}/`,
+    method: 'GET',
+    params:{
+      page:params.page_num,
+      page_size:params.page_size
+    }
+  });
+};
+
+export const commentPostAPI = (params) => {
+  return request({
+    url: `/api/post/comment/new/`,
+    method: 'POST',
+    data: {
+      id:params.id,
+      content: params.content,
+      reply_to_id: params.parent_id,
+      under_comment_id: params.under_comment_id
+    }
+  })
+}
 
 export const likeCommentAPI = (params) => {
   return request({
